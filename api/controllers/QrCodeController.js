545cde4bec49757004cf127a13bd26d3c58ca031
Ahmed Hosni <ahmedhosni.contact@gmail.com>
2016-09-16 14:39:28 +0000

    add: random hash gen

--- api/controllers/QrCodeController.js
+++ api/controllers/QrCodeController.js
@@ -7,26 +7,24 @@
 
 module.exports = {
     generate: function(req,res){
-        var value = "aef123456";
         User.findOne({id:1}).exec(function (err, user){
           if (!err) {
             console.log(user);
-            var qr = {value:value,owner:user};
+            var qr = {owner:user};
             QrCode.create(qr).exec(function(err, qrCode) {
                 if(!err){
                     return res.json(qr);
                 }else{
                     console.log(err);
                     res.status(404);
-                    return res.json({error:"Could not find user."});
+                    return res.json({error:"Could not generate QR Code."});
                 }
             });
           }else{
             console.log(err);
             res.status(404);
-            return res.json({error:"Could not generate QR Code."});
+            return res.json({error:"Could not find user."});
           }
         });
     }
 };
-
