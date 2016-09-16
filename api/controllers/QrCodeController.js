644328ecb9b254505846ccc84a1a2797b2694895
phoewass <phoewass@users.noreply.github.com>
2016-09-16 16:51:51 +0100

    add: qr code hash validation

--- api/controllers/QrCodeController.js
+++ api/controllers/QrCodeController.js
@@ -7,9 +7,8 @@
 
 module.exports = {
     generate: function(req,res){
-        User.findOne({id:1}).exec(function (err, user){
+        User.findOne(1).exec(function (err, user){
           if (!err) {
-            console.log(user);
             var qr = {owner:user};
             QrCode.create(qr).exec(function(err, qrCode) {
                 if(!err){
@@ -25,6 +24,28 @@ module.exports = {
             res.status(404);
             return res.json({error:"Could not find user."});
           }
+        });
+    },
+    validate: function(req,res){
+        var hash = req.params.hash;
+        QrCode.findOne({hash:hash}).exec(function(err, qrCode) {
+            if(!err){
+                var followerId = 2;
+                User.findOne(followerId).exec(function(err, follower) {
+                    if(!err){
+                        follower.unlock(qrCode.owner);
+                        return res.json({message:"User unlocked "+qrCode.owner});
+                     }else{
+                        console.log(err);
+                        res.status(404);
+                        return res.json({error:"Could not find owner User."});
+                    }
+                });
+            }else{
+                console.log(err);
+                res.status(404);
+                return res.json({error:"Could not find QR Code."});
+            }
         });
     }
 };
