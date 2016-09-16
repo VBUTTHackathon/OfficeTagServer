337439f272746b6530069a0d29e3c5e19372f34c
phoewass <phoewass@users.noreply.github.com>
2016-09-16 12:42:57 +0100

    add: store user when generate qr code

--- api/controllers/QrCodeController.js
+++ api/controllers/QrCodeController.js
@@ -8,9 +8,24 @@
 module.exports = {
     generate: function(req,res){
         var value = "aef123456";
-        QrCode.create({value:value,ownerId:"wassim"});
-        QrCode.findOne({value:value}).then(function(qr) {
-            res.json(qr.toJSON());
+        User.findOne({id:1}).exec(function (err, user){
+          if (!err) {
+            console.log(user);
+            var qr = {value:value,owner:user};
+            QrCode.create(qr).exec(function(err, qrCode) {
+                if(!err){
+                    return res.json(qr);
+                }else{
+                    console.log(err);
+                    res.status(404);
+                    return res.json({error:"Could not find user."});
+                }
+            });
+          }else{
+            console.log(err);
+            res.status(404);
+            return res.json({error:"Could not generate QR Code."});
+          }
         });
     }
 };
